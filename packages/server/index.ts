2878d0c7467d45bbab61820f054cfa938e5c052f
Arash <arash8877@gmail.com>
2025-10-06 20:30:13 +0200

    create a chat endpoint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import openai from 'openai';
 
 dotenv.config();
 
+const client = new openai.OpenAI({
+   apiKey: process.env.OPENAI_API_KEY || '',
+});
+
 const app = express();
+app.use(express.json());
+
+// Set the port
 const port = process.env.PORT || 3000;
 
 // routes
@@ -14,6 +22,18 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello from the API!' });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const { prompt } = req.body;
+
+   const response = await client.responses.create({
+      model: 'gpt-4o',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+   });
+   res.json({ message: response.output_text });
 });
 
 // Start the server
