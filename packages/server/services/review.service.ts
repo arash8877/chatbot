76e70a75f1f54a96e26c9f59386aa538037071ca
Arash <arash8877@gmail.com>
2025-10-10 17:16:53 +0200

    generating summarize with ai

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,3 +1,4 @@
+import openai from 'openai';
 import { type Review } from '../generated/prisma';
 import { reviewRepository } from '../repositories/review.repository';
 
@@ -10,7 +11,23 @@ export const reviewService = {
       // get the last 10 reviews
       const reviews = await reviewRepository.getReviews(productId, 10);
       const joinedReviews = reviews.map((r) => r.content).join('\n\n');
-      const summary = "This is the summary of the reviews."
-      return summary;
-   }
+
+      // send the reviews to LLM to summarize
+      const prompt = `Summarize the following customer reviews in to a short paragraph. Highlighting key themes, both positive and negative: ${joinedReviews} `;
+      const response = await client?.responses.create({
+         model: 'gpt-4.1',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 500,
+      })
+      return response?.output_text ?? '';
+   },
 };
+
+// const client = new openai.OpenAI({
+// apiKey: process.env.OPENAI_API_KEY,
+// });
+
+const client = process.env.OPENAI_API_KEY // alternative for no api key
+   ? new openai({ apiKey: process.env.OPENAI_API_KEY })
+   : null;
