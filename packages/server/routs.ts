12f8548f6919de35b7283944c53be09df138c075
Arash <arash8877@gmail.com>
2025-10-10 12:40:12 +0200

    create api to fetch reviews

--- packages/server/routs.ts
+++ packages/server/routs.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import type { Request, Response } from 'express';
 import { chatController } from './controllers/chat.controller';
+import { PrismaClient } from './generated/prisma';
 
 const router = Router();
 
@@ -16,5 +17,21 @@ router.get('/api/hello', (req: Request, res: Response) => {
 
 // Chat endpoint
 router.post('/api/chat', chatController.sendMessage);
+
+// kkk
+router.get('/api/products/:id/reviews', async (req:Request, res:Response) => {
+   const prisma = new PrismaClient();
+   const productId = Number(req.params.id)
+
+   if (isNaN(productId)) {
+      return res.status(400).json({ error: 'Invalid product ID' });
+   }
+
+   const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc' }
+   })
+   res.json(reviews)
+} )
 
 export default router;
