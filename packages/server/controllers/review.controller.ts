138b28a244c1202bd01deb0350f17438c4e0d77f
Arash <arash8877@gmail.com>
2025-10-13 13:27:01 +0200

    handling errors if now product exist

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 import { reviewService } from '../services/review.service';
+import { productRepository } from '../repositories/product.repository';
+import { reviewRepository } from '../repositories/review.repository';
 
 export const reviewController = {
    async getReviews(req: Request, res: Response) {
@@ -7,6 +9,17 @@ export const reviewController = {
 
       if (isNaN(productId)) {
          return res.status(400).json({ error: 'Invalid product ID' });
+      }
+
+      const product = await productRepository.getProduct(productId);
+
+      if (!product) {
+         return res.status(400).json({ error: 'Product not found' });
+      }
+
+      const checkThereIsReviews = await reviewRepository.getReviews(productId, 1);
+      if (checkThereIsReviews.length === 0) {
+         return res.status(400).json({ error: 'There are no review for summarize' });
       }
 
       const reviews = await reviewService.getReviews(productId);
