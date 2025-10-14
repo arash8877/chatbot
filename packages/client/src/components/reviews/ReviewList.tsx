0f89aa1debdcda7f6d35b141651d1cb68663a1df
Arash <arash8877@gmail.com>
2025-10-14 15:03:19 +0200

    refactoring with useMutation

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
 import axios from 'axios';
 import StarRating from './StarRating';
 import { HiSparkles } from 'react-icons/hi2';
 import ReviewSkeleton from './ReviewSkeleton';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { Button } from '../ui/button';
 
 type ReviewListProps = {
@@ -29,8 +28,6 @@ type SummarizeResponseProps = {
 
 //---------------------------- Main Function ----------------------------//
 const ReviewList = ({ productId }: ReviewListProps) => {
-   const [summary, setSummary] = useState('');
-   const [isSummaryLoading, setIsSummaryLoading] = useState(false);
    const {
       data: reviewData,
       isLoading,
@@ -40,12 +37,24 @@ const ReviewList = ({ productId }: ReviewListProps) => {
       queryFn: () => fetchReviews(),
    });
 
+   const {
+      mutate: handleSummarize,
+      isPending: isSummaryLoading,
+      isError: isSummaryError,
+      data: summarizeResponse,
+   } = useMutation<SummarizeResponseProps>({
+      mutationFn: () => summarizeReviews(),
+   });
+
    // By using React Query, we no longer need to manage state and side effects manually
    // React Query also manage caching, refetching 3 times, error handling, and background updates behind the scenes
 
    //    const [reviewData, setReviewData] = useState<GetReviewsResponseProps>();
    //    const [isLoading, setIsLoading] = useState(false);
    //    const [error, setError] = useState('');
+   //    const [summary, setSummary] = useState('');
+   //    const [isSummaryLoading, setIsSummaryLoading] = useState(false);
+   //    const [summaryError, setSummaryError] = useState('');
 
    //    const fetchReviews = async () => {
    //       try {
@@ -66,14 +75,28 @@ const ReviewList = ({ productId }: ReviewListProps) => {
    //       fetchReviews();
    //    }, []);
 
-   const handleSummarize = async () => {
-      setIsSummaryLoading(true);
+   // const handleSummarize = async () => {
+   //    try {
+   //       setIsSummaryLoading(true);
+   //       setSummaryError('');
 
+   //       const { data } = await axios.post<SummarizeResponseProps>(
+   //          `/api/products/${productId}/reviews/summarize`
+   //       );
+   //       setSummary(data.summary);
+   //    } catch (error) {
+   //       console.log(error);
+   //       setSummaryError('Could not generate the summary. Try again!');
+   //    } finally {
+   //       setIsSummaryLoading(false);
+   //    }
+   // };
+
+   const summarizeReviews = async () => {
       const { data } = await axios.post<SummarizeResponseProps>(
          `/api/products/${productId}/reviews/summarize`
       );
-      setSummary(data.summary);
-      setIsSummaryLoading(false);
+      return data;
    };
 
    const fetchReviews = async () => {
@@ -101,7 +124,7 @@ const ReviewList = ({ productId }: ReviewListProps) => {
       return <div>There is no review for this product!</div>;
    }
 
-   const currentSummary = reviewData?.summary || summary;
+   const currentSummary = reviewData?.summary || summarizeResponse?.summary;
 
    return (
       <div>
@@ -110,7 +133,11 @@ const ReviewList = ({ productId }: ReviewListProps) => {
                <p>{currentSummary}</p>
             ) : (
                <div>
-                  <Button onClick={handleSummarize} className='cursor-pointer' disabled={isSummaryLoading}>
+                  <Button
+                     onClick={() => handleSummarize()}
+                     className="cursor-pointer"
+                     disabled={isSummaryLoading}
+                  >
                      <HiSparkles />
                      Summarize
                   </Button>
@@ -118,6 +145,11 @@ const ReviewList = ({ productId }: ReviewListProps) => {
                      <div className="py-3">
                         <ReviewSkeleton />
                      </div>
+                  )}
+                  {isSummaryError && (
+                     <p className="text-red-500">
+                        Could not summarize the reviews. Try again!
+                     </p>
                   )}
                </div>
             )}
