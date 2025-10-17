82404e43499c02710f61afa80232d987ef86b118
Arash <arash8877@gmail.com>
2025-10-17 12:26:51 +0200

    fixed style bug

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
    return (
       <Router>
          <Navbar />
-         <main className="p-4 h-[calc(100vh-7rem)] max-full">
+         <main className="h-[calc(100vh-7rem)] max-full">
             <Routes>
                <Route path="/" element={<Navigate to="/chatbot" />} />
                <Route path="/chatbot" element={<Chatbot />} />
